ee73a89d688f2950743a51e37c36e1e8747c7ef9
katnow <kat.nowogrodzka@gmail.com>
2024-09-25 20:51:07 +0200

    about component

---
+++ app/components/About/index.tsx
@@ -0,0 +1,12 @@
+import styles from './About.module.scss';
+
+const About = () => {
+    return <div className={styles.about}>
+        <p>Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a huge corporation, and a digital product studio.</p>
+        <p>My main focus these days is building accessible user interfaces for our customers at Klaviyo. I most enjoy building software in the sweet spot where design and engineering meet — things that look good but are also built well under the hood. In my free time, </p>
+        <p>I've also released an online video course that covers everything you need to know to build a web app with the Spotify API.
+        </p>
+    </div>
+}
+
+export default About;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import styles from "./page.module.scss";
 import Intro from "@/app/components/Intro";
 import Menu from "@/app/components/Menu";
+import About from "@/app/components/About";
+import Experience from "@/app/components/Experience";
 
 export default function Home() {
   return (
@@ -9,6 +11,10 @@ export default function Home() {
             <div className={styles.left}>
                 <Intro />
                 <Menu />
+            </div>
+            <div className={styles.right}>
+                <About/>
+                <Experience />
             </div>
         </div>
     </div>
